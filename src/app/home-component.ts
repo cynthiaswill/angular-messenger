5ba9c9669cf728219fa9905c39c155dad87b309f
yizhang <yi.zhang@deltacapita.com>
2022-02-23 05:41:57 +0000

    sets up home login page

--- src/app/home-component.ts
+++ src/app/home-component.ts
@@ -1,7 +1,45 @@
 import { Component } from '@angular/core';
+import io from 'socket.io-client';
+
+interface JoinedSession {
+  username: string;
+  roomname: string;
+}
 
 @Component({
   selector: 'home',
-  template: '<h2>welcome to angular messenger</h2>',
+  template: `
+    <div>
+      <h1>Welcome to Angular Chat</h1>
+      <input type="text" [value]="username" (change)="onNameChange($event)" />
+      <input type="text" [value]="roomname" (change)="onRoomChange($event)" />
+      <button (click)="onClick()">Join</button>
+    </div>
+  `,
 })
-export class HomeCompnent {}
+export class HomeCompnent {
+  username: string = 'display name';
+  roomname: string = 'room name';
+
+  onNameChange($event: any) {
+    this.username = $event.target.value;
+    console.log('new username: ', this.username);
+  }
+
+  onRoomChange($event: any) {
+    this.roomname = $event.target.value;
+    console.log('new room: ', this.roomname);
+  }
+
+  onClick() {
+    if (this.username !== '' && this.roomname !== '') {
+      io().emit('joinRoom', <JoinedSession>{
+        username: this.username,
+        roomname: this.roomname,
+      });
+    } else {
+      alert('username and roomname cannot be empty !');
+      window.location.reload();
+    }
+  }
+}
