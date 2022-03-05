5d2771b3fc8a052df7d016c345dcedde4f41974f
Yi Zhang <90160138+cynthiaswill@users.noreply.github.com>
2022-03-05 03:07:00 +0000

    adds type variables in http service

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +9,7 @@ export class MessagesService {
   private baseUrl: string = `Https://nc-games-board.herokuapp.com/api`;
   constructor(private http: HttpClient) {}
 
-  getMessages(roomName: string) {
-    return this.http.get(this.baseUrl + '/messages' + `/${roomName}`);
+  getMessages<T>(roomName: string): Observable<T> {
+    return this.http.get<T>(this.baseUrl + '/messages' + `/${roomName}`);
   }
 }
