7c8c1a55195e67abed4caf6865aa21e4efdb4945
Yi Zhang <90160138+cynthiaswill@users.noreply.github.com>
2022-03-05 03:14:34 +0000

    extracts routing module into its own file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { HomeCompnent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeCompnent },
+  { path: 'chat', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,13 @@ import { HomeCompnent } from './home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, HomeCompnent, ChatComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeCompnent },
-      { path: 'chat', component: ChatComponent },
-    ]),
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [TransferService, MessagesService, SocketService],
   bootstrap: [AppComponent],
 })
